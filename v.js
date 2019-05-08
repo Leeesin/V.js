ef50abc35858fea86f1946b0d8b2396f53b0e669
qinjialei <954551222@qq.com>
2019-05-08 19:42:28 +0800

    实现v-show 指令

--- v.js
+++ v.js
@@ -13,17 +13,13 @@ function defineReactive(obj, key, val, cb) {
   });
 }
 
-
 class V {
   constructor({ data = {}, methods = {} }) {
     this._data = data
     this._methods = methods
     this.observe(this._data, this.notify)
-
-    this._data.xxx2 = '222'
-    // setTimeout(() => {
-    //   this._data.xxx2 = '333333333'
-    // }, 1000);
+    this.allDirective = this.genDirectiveObj()
+    // this._data.xxx = 111111
   }
 
   observe(obj, cb) {
@@ -34,35 +30,55 @@ class V {
 
 
   notify(key, newValue) {//拿到更新的 key 和 value
-    const allDirective = this.genDirectiveObj()
-    const deps = allDirective[key] //需要更新的节点
+    // const allDirective = this.genDirectiveObj()
+    const deps = this.allDirective[key] //需要更新的节点
     if (!deps) return
     deps.forEach(item => {
       item.update(newValue)
     })
   }
 
-
-  genDirectiveObj(params) {//获取带有指令的节点 生成指令信息的对象集合   
+  genDirectiveObj() {//获取带有指令的节点 生成指令信息的对象集合   
     const directives = '[v-text],[v-model],[v-show],[v-click]'
     var els = document.querySelectorAll(directives)
     var c = Array.from(els)
     var directiveMap = {}
     c.forEach(item => {
+      const defaultDisplay = window.getComputedStyle(item).display  //元素默认的 display
       var c2 = Array.from(item.attributes)
       c2.forEach(item2 => {
-        console.log('item2.name 的值是：', item2.name);
         if (item2.name === 'v-click') { //如果是事件
-
           item.addEventListener('click', this._methods[item2.value].bind(this._data))
-
         }
+
+        if (item2.name === 'v-text') {
+          if (this._data[item2.value] === undefined) {
+            console.error('模板内的变量必须在data内申明！')
+          } else {
+            item.innerHTML = this._data[item2.value]
+          }
+        }
+
+        if (item2.name === 'v-show') {
+          if (this._data[item2.value] === undefined) {
+            console.error('模板内的变量必须在data内申明！')
+          } else {
+            if (this._data[item2.value]) {
+              if (defaultDisplay === 'block') item.style.display = defaultDisplay
+              if (defaultDisplay === 'inline-block') item.style.display = defaultDisplay
+              if (defaultDisplay === 'inline') item.style.display = defaultDisplay
+            }
+            else {
+              item.style.display = 'none'
+            }
+          }
+        }
+
         const isDirective = directives.match(item2.name) //是否是指令属性 
         if (isDirective) {
-
-
           directiveMap[item2.value] = []
           directiveMap[item2.value].push({
+            defaultDisplay,
             el: item,
             directiveName: item2.name,
             update(newValue) {
@@ -72,18 +88,24 @@ class V {
               if (this.directiveName === 'v-model') {
                 this.el.value = newValue
               }
+              if (this.directiveName === 'v-show') {
+                console.log(11111);
+
+                if (newValue) {
+                  if (this.defaultDisplay === 'block') this.el.style.display = this.defaultDisplay
+                  if (this.defaultDisplay === 'inline-block') this.el.style.display = this.defaultDisplay
+                  if (this.defaultDisplay === 'inline') this.el.style.display = this.defaultDisplay
+                } else {
+                  this.el.style.display = 'none'
+                }
+              }
+
             }
           })
+
         }
       })
     })
     return directiveMap
-
-    console.log('els 的值是：', els);
-    console.log(' c 的值是：', c);
-    console.log('directiveMap 的值是：', directiveMap);
-
   }
-
-
 }
